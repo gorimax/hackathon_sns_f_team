af54793ea0ff824cb2e708dcd622bf11d870e207
rqime <kimikisk@gmail.com>
2026-01-24 03:11:55 +0900

    flaskの環境構築

--- SNSApp/app.py
+++ SNSApp/app.py
@@ -1,1 +1,16 @@
-from flask import Flask, request, redirect, render_template, session, flash, abort, url_for
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # ここは login を見せたいなら login に飛ばすのもアリ
+    return render_template("post/base.html")
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("auth/login.html")
+
+@app.route("/signup", methods=["GET"])
+def signup():
+    return render_template("auth/signup.html")
