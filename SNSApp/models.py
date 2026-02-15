28ece6156f253178d55c51f11d6645bca90602a4
GORI <trek6117@gmail.com>
2026-02-15 19:22:14 +0900

    Commentクラスを追加

--- SNSApp/models.py
+++ SNSApp/models.py
@@ -171,3 +171,62 @@ class Post:
             abort(500)
         finally:
             db_pool.release(conn)
+
+class Comment:
+    @classmethod
+    def create(cls, user_id, post_id, content):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO comments (user_id, post_id, content) VALUES (%s, %s, %s);"
+                cur.execute(sql, (user_id, post_id, content))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def get_by_post_id(cls, post_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM comments WHERE post_id=%s ORDER BY created_at DESC;"
+                cur.execute(sql, (post_id,))
+                comments = cur.fetchall()
+            return comments
+        except pymysql.Error as e:
+            print(f'エラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def find_by_id(cls, comment_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM comments WHERE comment_id=%s AND deleted_at IS NULL;"
+                cur.execute(sql, (comment_id,))
+                post = cur.fetchone()
+            return post
+        except pymysql.Error as e:
+            print(f'エラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def delete(cls, comment_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "UPDATE comments SET deleted_at = NOW() WHERE comment_id=%s;"
+                cur.execute(sql, (comment_id))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
